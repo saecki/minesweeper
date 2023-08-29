a7d3717546940f91d616c06793d3df6c533db550
Saecki <Saecki@users.noreply.github.com>
2023-08-29 13:08:24 +0000

    deploy: f18df43250702a8e6b257afa23d944a037d820c8

--- minesweeper_web-38acc4d6d5c2d7b8.js
+++ minesweeper_web-38acc4d6d5c2d7b8.js
@@ -1395,7 +1395,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('minesweeper_web-b3fec9c1fed546c3_bg.wasm', import.meta.url);
+        input = new URL('minesweeper_web-38acc4d6d5c2d7b8_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
